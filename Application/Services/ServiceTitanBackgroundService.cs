37b53355fdededd19095c6ddecaafb5f9281aabf
PatriotMechanical <brandon@patriotmechanicaltx.com>
2026-03-10 08:47:31 -0500

    Fix SyncAppointmentsAsync → SyncAppointmentsAndAutoBoardAsync in BackgroundService

--- Application/Services/ServiceTitanBackgroundService.cs
+++ Application/Services/ServiceTitanBackgroundService.cs
@@ -31,7 +31,7 @@ namespace PatriotMechanical.API.Application.Services
 
                         await engine.SyncCustomersAsync(fullSync: false);
                         await engine.SyncJobsAsync(fullSync: false);
-                        await engine.SyncAppointmentsAsync();
+                        await engine.SyncAppointmentsAndAutoBoardAsync();
                         await engine.SyncEstimatesAsync();
                     }
                 }
